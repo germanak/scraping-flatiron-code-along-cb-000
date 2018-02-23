51d59c48a0ca5c45b6c8afa4801612349768f493
Alec German <alec_german@yahoo.com>
2018-02-23 03:27:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,7 +30,7 @@ class Scraper
       course = Course.new
       course.title = post.css('h2').text
       course.schedule = post.css('.date').text
-      course.title = post.css('p').text
+      course.description = post.css('p').text
     end
   end
 end
